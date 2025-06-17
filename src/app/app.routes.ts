f686f2f7d604e73667ab5aae2f077662a11f873d
wb <carloseguibegui@gmail.com>
2025-06-17 17:33:41 -0300

    plaza_hotel_tandil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,10 +8,9 @@ import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
+  { path: 'menuonline/:cliente', pathMatch: 'full', redirectTo: 'menuonline/:cliente/carta'},
   { path: 'menuonline/:cliente/carta', component: CartaComponent },
-  {
-    path: 'menuonline/:cliente/carta/:categoria', component: CategoriaComponent, runGuardsAndResolvers: 'always'
-  },
+  { path: 'menuonline/:cliente/carta/:categoria', component: CategoriaComponent, runGuardsAndResolvers: 'always'},
   { path: 'auth/login', component: LoginComponent },
   { path: 'admin', component: AdminComponent, canActivate: [authGuard] }, // Ruta protegida
   { path: '**', component: InicioComponent }
