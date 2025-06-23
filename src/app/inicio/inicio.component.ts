23aae4c2aeaccfc1b8e6781c179a958c4fc9332a
wb <carloseguibegui@gmail.com>
2025-06-23 13:21:13 -0300

    prego y tramonto

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,17 +1,49 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { CommonModule, NgIf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf, NgStyle,CommonModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrl: './inicio.component.css',
+  animations: [
+    trigger('fadeBanner', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('800ms ease-in-out', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('800ms ease-in-out', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit, OnDestroy {
   isScrolled = false;
+  bannerImages = [
+    { url: 'https://firebasestorage.googleapis.com/v0/b/menu-digital-e8e62.firebasestorage.app/o/landing%2F1.webp?alt=media', route: '/menuonline/requeterico/carta' },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/menu-digital-e8e62.firebasestorage.app/o/landing%2F2.webp?alt=media', route: '/menuonline/demo_hotel/carta' },
+    { url: 'https://firebasestorage.googleapis.com/v0/b/menu-digital-e8e62.firebasestorage.app/o/landing%2F3.webp?alt=media', route: '/menuonline/demo_cafeteria/carta' },
+  ];
+  bannerIndex = 0;
+  bannerInterval: any;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY >= 56;
+  }
+
+  ngOnInit() {
+    this.bannerInterval = setInterval(() => {
+      this.bannerIndex = (this.bannerIndex + 1) % this.bannerImages.length;
+    }, 6000);
+  }
+
+  ngOnDestroy() {
+    if (this.bannerInterval) {
+      clearInterval(this.bannerInterval);
+    }
   }
 }
